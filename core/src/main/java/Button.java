c69e8d5857d1f4973f686dff8264c641f6688645
polo2325 <flipflop2325@outlook.com>
2024-12-04 13:30:22 -0500

    work in progress

--- core/src/main/java/Button.java
+++ core/src/main/java/Button.java
@@ -1,3 +1,40 @@
-public class Button {
-    
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+
+public class Button extends Game{
+
+    Stage stage;
+    TextButton button;
+    TextButtonStyle textButtonStyle;
+    TextureAtlas buttonAtlas;
+
+    @Override
+    public void create() {      
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+     
+        textButtonStyle = new TextButtonStyle();
+        button = new TextButton("Button1", textButtonStyle);
+        stage.addActor(button);
+    }
+
+    @Override
+    public void render() {      
+        super.render();
+        stage.draw();
+    }
 }
+
+//  button.addListener(new ChangeListener() {
+//         @Override
+//         public void changed (ChangeEvent event, Actor actor) {
+//             System.out.println("Button Pressed");
+//         }
+//     });
+// }
