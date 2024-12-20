af11d0c29e7f2b954b6b7e72f16a654713da9d71
ilikecandy <jacobnaonao7@gmail.com>
2024-12-20 12:13:04 -0500

    Added all the pvz fonts, finally got the right one for the numbers

--- core/src/main/java/ca/codepet/ui/PlantBar.java
+++ core/src/main/java/ca/codepet/ui/PlantBar.java
@@ -1,12 +1,12 @@
 package ca.codepet.ui;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
 public class PlantBar {
     private Texture barTexture;
@@ -18,16 +18,15 @@ public class PlantBar {
     public PlantBar(int sun) {
         barTexture = new Texture("ui-components/bar.png");
         batch = new SpriteBatch();
-        
+
         // Load font
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
-            Gdx.files.internal("fonts/fbUsv8C5eIBlack.ttf")
-        );
+                Gdx.files.internal("fonts/contb.ttf"));
         FreeTypeFontParameter parameter = new FreeTypeFontParameter();
-        parameter.size = 24;
+        parameter.size = 20;
+        parameter.color.set(0, 0, 0, 1);
         font = generator.generateFont(parameter);
         // dispose generator
-        generator.dispose();
 
         sunDisplay = sun;
     }
@@ -45,13 +44,16 @@ public class PlantBar {
         batch.begin();
         // Draw bar
         batch.draw(barTexture, 0, barY, newWidth, newHeight);
-        
+
         // Draw sun count
         String sunText = String.valueOf(sunDisplay);
-        float textX = 10;
-        float textY = barY + newHeight - 10;
+        float centerX = 45; // Original x position
+        GlyphLayout layout = new GlyphLayout(font, sunText); // To measure the text
+        float textWidth = layout.width;
+        float textX = centerX - (textWidth / 2); // Center around the original x position
+        float textY = barY + newHeight - 75;
         font.draw(batch, sunText, textX, textY);
-        
+
         batch.end();
     }
 
