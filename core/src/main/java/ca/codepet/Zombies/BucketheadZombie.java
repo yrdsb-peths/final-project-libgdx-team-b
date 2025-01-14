4bddae60fbd5bde1ccd342c2b8ee366f92008c5c
polo2325 <flipflop2325@outlook.com>
2025-01-14 14:38:54 -0500

    change stats for bucketheadzombie

--- core/src/main/java/ca/codepet/Zombies/BucketheadZombie.java
+++ core/src/main/java/ca/codepet/Zombies/BucketheadZombie.java
@@ -7,6 +7,6 @@ import ca.codepet.worlds.DayWorld;
 
 public class BucketheadZombie extends Zombie {
     public BucketheadZombie(DayWorld theWorld) {
-         super(theWorld, new Texture("images/bucketHeadZombie.png"), 100, 1, 2.0f);
+         super(theWorld, new Texture("images/bucketHeadZombie.png"), 300, 50, 2.0f);
     }
 }
